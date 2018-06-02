529aa49371c327ad7e03f8a4b656ae3c2c0bed1a
Vladimir Jelincic <vladimir.jelincic@gmail.com>
2018-06-02 15:46:30 +0200

    Travis config

--- config/ci.js
+++ config/ci.js
@@ -1,8 +1,7 @@
 module.exports = {
     googleClientID: '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
     googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-    mongoURI: 'mongodb://vladimir:password@ds219000.mlab.com:19000/blog_dev_vj',
-    //mongoURI: 'mongodb://localhost:27017/blog_dev',
+    mongoURI: 'mongodb://localhost:27017/blog_dev',
     cookieKey: '123123123',
     redisUrl: 'redis://127.0.0.1:6379'
 };
